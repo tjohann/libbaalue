2773ac4be5f7826e121164fc41eed370640e2ef5
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2016-12-23 18:16:00 +0100

    another fix for the __DEBUG__ handling

--- do_all_in_one_step.sh
+++ do_all_in_one_step.sh
@@ -5,8 +5,8 @@ make distclean
 
 autoreconf --install || exit 1
 
-# ./configure --enable-debug --enable-examples --enable-scripts --enable-lcd160x --disable-silent-rules --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib
-./configure --enable-debug --enable-examples --enable-lcd160x --prefix=/usr/local
+# ./configure --enable-debug-info --enable-examples --enable-scripts --enable-lcd160x --disable-silent-rules --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib
+./configure --enable-debug-info --enable-examples --enable-lcd160x --prefix=/usr/local
 
 cd po
 make update-po
