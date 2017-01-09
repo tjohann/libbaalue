f67c20b6355e67796a8ce4e5aeb5ff0bd90fa1db
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2017-01-09 16:53:31 +0100

    change installation target in do_all_in_... script

--- do_all_in_one_step.sh
+++ do_all_in_one_step.sh
@@ -6,7 +6,7 @@ make distclean
 autoreconf --install || exit 1
 
 # ./configure --enable-debug-info --enable-examples --enable-scripts --enable-lcd160x --disable-silent-rules --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib
-./configure --enable-debug-info --enable-examples --enable-lcd160x --prefix=/usr/local
+./configure --enable-debug-info --enable-examples --enable-lcd160x
 
 cd po
 make update-po
