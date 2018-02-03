8a36fc06a633ad277093cbabe53ba058c2db6947
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2018-02-03 12:03:11 +0100

    update year info in header update pot file

--- src/network.c
+++ src/network.c
@@ -1,6 +1,6 @@
 /*
   GPL
-  (c) 2016, thorsten.johannvorderbrueggen@t-online.de
+  (c) 2016-2018, thorsten.johannvorderbrueggen@t-online.de
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
